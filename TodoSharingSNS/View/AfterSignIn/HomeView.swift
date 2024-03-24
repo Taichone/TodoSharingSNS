e2aa56a43fe6d015ed20a07b3cf139f7f369f737
Taichone <taichone.dev@gmail.com>
2024-03-24 10:36:36 +0900

    Revert "Revert "add: HomeView は TabView を採用""
    This reverts commit 908213c0af1e24684ba0423194cc3eaef892450e.

--- TodoSharingSNS/View/AfterSignIn/HomeView.swift
+++ TodoSharingSNS/View/AfterSignIn/HomeView.swift
@@ -9,15 +9,26 @@ import SwiftUI
 
 struct HomeView: View {
     var userManager: UserManager
+    @State private var selectedTab = 1
     
     var body: some View {
-        VStack {
-            Text("You're logged in.")
-                .font(.title)
-                .padding()
+        TabView(selection: self.$selectedTab) {
+            Text("Friend List View").tabItem { Image(systemName: "person.2.fill") }.tag(1)
+            Text("My Todo List View").tabItem { Image(systemName: "list.bullet") }.tag(2)
             Button("Log Out") {
                 self.userManager.signOut()
-            }
+            }.tabItem { Image(systemName: "gear") }.tag(3)
         }
     }
+}
+
+fileprivate struct HomeViewWrapper: View {
+    @State private var userManager = UserManager()
+    var body: some View {
+        HomeView(userManager: self.userManager)
+    }
+}
+
+#Preview {
+    HomeViewWrapper()
 }
