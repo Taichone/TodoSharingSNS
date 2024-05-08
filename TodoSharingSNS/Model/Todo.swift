e20daa3ff5ac71d16f341ece88753b00b75a5bc7
Taichone <taichone.dev@gmail.com>
2024-05-08 20:00:36 +0900

    add: MOCK_TODOS の追加

--- TodoSharingSNS/Model/Todo.swift
+++ TodoSharingSNS/Model/Todo.swift
@@ -29,7 +29,10 @@ extension Todo {
         .init(title: "水やり", deadline: .allday, completed: false),
         .init(title: "買い物", deadline: .morning, completed: true),
         .init(title: "携帯の修理", deadline: .afternoon, completed: false),
-        .init(title: "テスト勉強", deadline: .tonight, completed: false)
+        .init(title: "ランボルギーニの洗車", deadline: .allday, completed: false),
+        .init(title: "フェラーリの車検", deadline: .morning, completed: true),
+        .init(title: "ワゴンRの改造", deadline: .afternoon, completed: false),
+        .init(title: "原付の改造", deadline: .tonight, completed: false)
     ]
 }
 
